58978956793d7eaf796754572108bd0de5e15f9b
User <t8140192@dias.aueb.gr>
2018-05-03 13:54:23 +0300

    Update setup

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-# rm -rf hatshufflepy/libhatshuffle/include/ hatshufflepy/libhatshuffle/build/ \
-# hatshufflepy/libhatshuffle/lib/
+rm -rf hatshufflepy/libhatshuffle/include/ hatshufflepy/libhatshuffle/build/ hatshufflepy/libhatshuffle/lib/
 rm -rf build dist hatshufflepy.egg-info
 rm *.json secret_key
+rm hatshufflepy.so
--- setup.py
+++ setup.py
@@ -1,47 +1,53 @@
 import os
-# from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
+from subprocess import call
+from setuptools.command.install import install
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
 
-os.environ['CC'] = 'g++'
-os.environ['CXX'] = 'g++'
+#  os.environ['CC'] = 'g++'
+#  os.environ['CXX'] = 'g++'
 
-pwd = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-hatshufflpy_module = Extension('hatshufflepy',
-                               sources = ['hatshufflepy/hatshufflepy.pyx'],
-                               language = 'c++',
-                               include_dirs = ['hatshufflepy/libhatshuffle/include/libhatshuffle',
-                                             'hatshufflepy/libhatshuffle/include',
-                                             'libhatshuffle/include/libff'],
-                               libraries = ['zm', 'ff', 'hatshuffle'],
-                               library_dirs = ['/usr/local/lib',
-                                               'hatshufflepy/libhatshuffle/lib'],
-                               runtime_library_dirs=[pwd +
-                                                     '/hatshufflepy/libhatshuffle/lib'],
-                               extra_compile_args = ['-std=c++11', '-fPIC',
-                                                     '-shared', '-w',
-                                                     '-static', '-O3'],
-                               extra_link_args = ['-lgmp', '-fopenmp', '-g'])
+
+class CustomBuild(install):
+    def run(self):
+        call(['bash', 'install_depends.sh'])
+        install.run(self)
+
+
+module = Extension('hatshufflepy',
+                    sources = ['hatshufflepy/hatshufflepy.pyx'],
+                    language = 'c++',
+                    include_dirs = ['hatshufflepy/libhatshuffle/include/libhatshuffle',
+                                    'hatshufflepy/libhatshuffle/include',
+                                    'libhatshuffle/include/libff'],
+                    libraries = ['zm', 'ff', 'hatshuffle'],
+                    library_dirs = ['/usr/local/lib',
+                                    'hatshufflepy/libhatshuffle/lib'],
+                    runtime_library_dirs=[ROOT_PATH +
+                                          '/hatshufflepy/libhatshuffle/lib'],
+                    extra_compile_args = ['-std=c++11', '-fPIC',
+                                          '-shared', '-w',
+                                          '-static', '-O3'],
+                    extra_link_args = ['-lgmp', '-fopenmp', '-g'])
+
 
 setup(
     name='hatshufflepy',
 	version='0.0.1',
-
+    packages=find_packages(),
 	author='Stefanos Chaliasos',
 	author_email='stefanoshaliassos@gmail.com',
 	description='Cython wrapper for hat shuffle implementation',
 	license='MIT',
-	keywords='mixnet',
-	url='https://github.com/grnet/hat_shuffle',
+	keywords='mixnet ecc',
+	url='https://github.com/StefanosChaliasos/hatshufflepy',
 
-    	install_requires=[
-		'cython >= 0.22.1'
-	],
-
-    ext_modules=cythonize(hatshufflpy_module),
-    cmdclass = {'build_ext': build_ext},
+    install_requires=["cython >= 0.22.1"],
+    ext_modules=cythonize(module),
+    cmdclass = {'build_ext': build_ext, 'install': CustomBuild},
 )
