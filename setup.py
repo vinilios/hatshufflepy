397aeb59193a578c8d73556638a23518b426d8c6
User <t8140192@dias.aueb.gr>
2018-05-03 13:22:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,41 +3,45 @@ import os
 from setuptools import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-
 from Cython.Distutils import build_ext
 
 
-os.environ["CC"] = "g++"
-os.environ["CXX"] = "g++"
+os.environ['CC'] = 'g++'
+os.environ['CXX'] = 'g++'
+
+pwd = os.path.dirname(os.path.abspath(__file__))
+
+hatshufflpy_module = Extension('hatshufflepy',
+                               sources = ['hatshufflepy/hatshufflepy.pyx'],
+                               language = 'c++',
+                               include_dirs = ['hatshufflepy/libhatshuffle/include/libhatshuffle',
+                                             'hatshufflepy/libhatshuffle/include',
+                                             'libhatshuffle/include/libff'],
+                               libraries = ['zm', 'ff', 'hatshuffle'],
+                               library_dirs = ['/usr/local/lib',
+                                               'hatshufflepy/libhatshuffle/lib'],
+                               runtime_library_dirs=[pwd +
+                                                     '/hatshufflepy/libhatshuffle/lib'],
+                               extra_compile_args = ['-std=c++11', '-fPIC',
+                                                     '-shared', '-w',
+                                                     '-static', '-O3'],
+                               extra_link_args = ['-lgmp', '-fopenmp', '-g'])
 
 setup(
-    name="hatshufflepy",
-	version="0.0.1",
+    name='hatshufflepy',
+	version='0.0.1',
 
-	author="Stefanos Chaliasos",
-	author_email="stefanoshaliassos@gmail.com",
-	description="Cython wrapper for hat shuffle implementation",
-	license="MIT",
-	keywords="mixnet",
-	url="https://github.com/grnet/hat_shuffle",
+	author='Stefanos Chaliasos',
+	author_email='stefanoshaliassos@gmail.com',
+	description='Cython wrapper for hat shuffle implementation',
+	license='MIT',
+	keywords='mixnet',
+	url='https://github.com/grnet/hat_shuffle',
 
     	install_requires=[
-		"cython >= 0.22.1"
+		'cython >= 0.22.1'
 	],
 
-    ext_modules=cythonize(
-        Extension(
-            "hatshufflepy",
-            sources=["hatshufflepy/hatshufflepy.pyx"],
-            language="c++",
-            include_dirs=["hatshufflepy/libhatshuffle/include/libhatshuffle",
-                          "hatshufflepy/libhatshuffle/include",
-                          "libhatshuffle/include/libff"],
-            library_dirs = ["/usr/local/lib",
-                            "hatshufflepy/libhatshuffle/lib"],
-            extra_compile_args = ["-std=c++11", "-fPIC", "-shared", "-w", "-static", "-O3"],
-            extra_link_args = ["-lgmp", "-lzm", "-lff", "-lhatshuffle", "-fopenmp", "-g"]
-        )
-    ),
+    ext_modules=cythonize(hatshufflpy_module),
     cmdclass = {'build_ext': build_ext},
 )
